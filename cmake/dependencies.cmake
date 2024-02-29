a99864443438e3775fc2dd03a1e160d37b286a92
Tobias Loew <tobias-loew@users.noreply.github.com>
2024-02-29 18:12:39 +0100

    Update dependencies.cmake

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -11,13 +11,13 @@ find_package(Boost)
 ###############################################################################
 # GoogleTest
 ###############################################################################
-#add_subdirectory(${CMAKE_SOURCE_DIR}/googletest-release-1.10.0)
-#target_include_directories(gtest      INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
-#target_include_directories(gtest_main INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
+add_subdirectory(${CMAKE_SOURCE_DIR}/googletest-release-1.10.0)
+target_include_directories(gtest      INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
+target_include_directories(gtest_main INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
 
 
 ###############################################################################
 # Google Benchmark
 ###############################################################################
-#add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark-v1.1.0)
-#target_include_directories(benchmark INTERFACE ${CMAKE_HOME_DIRECTORY}/benchmark-v1.1.0/include)
+add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark-v1.1.0)
+target_include_directories(benchmark INTERFACE ${CMAKE_HOME_DIRECTORY}/benchmark-v1.1.0/include)
