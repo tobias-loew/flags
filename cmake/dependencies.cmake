af9da9391871d1fdf2d11cfc62ebf6bdd62c6753
Tobias Loew <tobi@die-loews.de>
2024-02-29 17:51:42 +0100

    cmake - no idea what I'm doing

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,23 @@
+# Copyright Louis Dionne 2016
+# Copyright Zach Laine 2016
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+###############################################################################
+# Boost
+###############################################################################
+find_package(Boost)
+
+###############################################################################
+# GoogleTest
+###############################################################################
+#add_subdirectory(${CMAKE_SOURCE_DIR}/googletest-release-1.10.0)
+#target_include_directories(gtest      INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
+#target_include_directories(gtest_main INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.10.0/googletest/include)
+
+
+###############################################################################
+# Google Benchmark
+###############################################################################
+#add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark-v1.1.0)
+#target_include_directories(benchmark INTERFACE ${CMAKE_HOME_DIRECTORY}/benchmark-v1.1.0/include)
